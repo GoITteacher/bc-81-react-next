bf00e9a847d3d74372be5b010bcd8cf4923dcfbe
Pashchenko Volodymyr <94864615+Pikimell@users.noreply.github.com>
2026-07-12 23:26:25 +0300

    Initialize 04-state

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,10 @@
+/**
+ * Заглушка акордеона: додайте типи пропсів, стан та обробники під час уроку.
+ */
+export default function Accordion() {
+  return (
+    <div>
+      <h3>Accordion</h3>
+    </div>
+  );
+}
