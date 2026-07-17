fe36b03ec4d7bb11f4ac0dcf4fac0d563a2191c2
Volodymyr Pashchenko <pikimelposhta@gmail.com>
2026-07-17 12:24:34 +0300

    update

--- src/components/ModalExample/ModalExample.tsx
+++ src/components/ModalExample/ModalExample.tsx
@@ -4,11 +4,36 @@ import css from "./ModalExample.module.css";
 
 const ModalExample = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
   };
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
   const closeModal = () => {
     setIsOpen(false);
   };
