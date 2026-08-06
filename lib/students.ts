8516ac8b64984b61ad9b6b20961930d68207fdfb
Volodymyr Pashchenko <pikimelposhta@gmail.com>
2026-08-06 09:24:00 +0300

    update

---
+++ lib/students.ts
@@ -0,0 +1,12 @@
+import { GetStudentsResponse, Student } from "@/types/students";
+import { globalApi } from "./serverConfig";
+
+export const getStudents = async () => {
+  const res = await globalApi.get<GetStudentsResponse>("/public/students");
+  return res.data;
+};
+
+export const getStudentById = async (id: string) => {
+  const res = await globalApi.get<Student>(`/public/students/${id}`);
+  return res.data;
+};
